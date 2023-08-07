84d0ccbab95931da16b28e232257638f6ebbac66
SK Verma <s.k.verma@live.in>
2023-08-07 15:33:55 +0530

    sendingToGitHub

--- routes/booking.js
+++ routes/booking.js
@@ -11,20 +11,92 @@ const config = {
   trustServerCertificate: true,
 };
 
-router.get('/:empId/:wpId/:dtId', async (req, res) => {
+router.get('/:empId/:dtId', async (req, res) => {
   try {
-    const { empId, wpId, dtId } = req.params;
+    const { empId, dtId } = req.params;
     const pool = await sql.connect(config);
     const result = await pool
       .request()
       .input('empId', sql.Int, empId)
-      .input('wpId', sql.Int, wpId)
       .input('dtId', sql.BigInt, dtId).query(`
-    SELECT booking FROM     bookings WHERE  (empId = @empId) AND (workPlanId = @wpId) AND (dateId = @dtId)
+      SELECT A.id as theWpId,
+	      theBooking = isnull((select C.booking from bookings C where ((C.workPlanId = A.id) and (C.dateId = @dtId))), ''),
+	      toUpd = (select count(*) from bookings C where ((C.workPlanId = A.id) and (C.dateId = @dtId)))
+      FROM     workPlan A INNER JOIN emp B ON A.depttId = B.curDeptt
+      WHERE  (B.id = @empId)
+      order by theWpId
       `);
     res.json(result.recordset);
   } catch (err) {
     console.error('Error fetching booking:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+// id	bigint
+// empId	int
+// workPlanId	int
+// dateId	bigint
+// booking	float
+
+// POST route to insert booking data
+router.post('/', async (req, res) => {
+  try {
+    // const { error } = validate(req.body);
+    // if (error)
+    //   return res.status(400).send(`Invalid input: ${error.details[0].message}`);
+
+    const { empId, workPlanId, dateId, booking } = req.body;
+
+    // Create a SQL Server connection pool
+    const pool = await sql.connect(config);
+
+    // Insert booking data into the bookings table
+    await pool
+      .request()
+      .input('empId', sql.Int, empId)
+      .input('workPlanId', sql.Int, workPlanId)
+      .input('dateId', sql.BigInt, dateId)
+      .input('booking', sql.Float, booking)
+      .query(
+        'INSERT INTO bookings (empId,workPlanId,dateId,booking) VALUES (@empId,@workPlanId,@dateId,@booking)'
+      );
+
+    // res;
+    res
+      .status(201)
+      .send(`Booking data inserted successfully ${JSON.stringify(req.body)}`);
+  } catch (err) {
+    console.error('Error inserting booking data:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+// PUT route to insert booking data
+router.put('/', async (req, res) => {
+  try {
+    // const { error } = validate(req.body);
+    // if (error)
+    //   return res.status(400).send(`Invalid input: ${error.details[0].message}`);
+
+    const { empId, workPlanId, dateId, booking } = req.body;
+
+    // Create a SQL Server connection pool
+    const pool = await sql.connect(config);
+
+    // Insert booking data into the bookings table
+    await pool
+      .request()
+      .input('empId', sql.Int, empId)
+      .input('workPlanId', sql.Int, workPlanId)
+      .input('dateId', sql.BigInt, dateId)
+      .input('booking', sql.Float, booking)
+      .query(
+        'UPDATE bookings set booking = @booking where ((empId = @empId) and (workPlanId = @workPlanId) and (dateId = @dateId))'
+      );
+
+    res;
+    res.send(`Booking data updated successfully ${JSON.stringify(req.body)}`);
+  } catch (err) {
+    console.error('Error updating booking data:', err);
     res.status(500).send('Internal Server Error');
   }
 });
